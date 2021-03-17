4a57b67e12276f683dc593151f50c748c5e92cca
simonkrupa <xkrupas@stuba.sk>
2021-03-17 12:08:37 +0100

    class game, player(vytovrenei hracov)

---
+++ src/main/java/sk/stuba/fei/uim/oop/Game.java
@@ -0,0 +1,20 @@
+package sk.stuba.fei.uim.oop;
+
+
+import java.util.Scanner;
+
+public class Game {
+    private int numberOfPlayers;
+    private Player players[];
+
+    public Game() {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter number of players: ");
+        numberOfPlayers = Integer.parseInt(myObj.nextLine());
+        players = new Player[numberOfPlayers];
+        for(int i = 0; i < numberOfPlayers; i++){
+            System.out.println("Name of player number "+ (i+1) + ": ");
+            players[i] = new Player(myObj.nextLine(), i);
+        }
+    }
+}
---
+++ src/main/java/sk/stuba/fei/uim/oop/Player.java
@@ -0,0 +1,15 @@
+package sk.stuba.fei.uim.oop;
+
+public class Player {
+    private String name;
+    private int numOfPlayer;
+
+    public Player(String name, int numOfPlayer) {
+        this.name = name;
+        this.numOfPlayer = numOfPlayer;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
