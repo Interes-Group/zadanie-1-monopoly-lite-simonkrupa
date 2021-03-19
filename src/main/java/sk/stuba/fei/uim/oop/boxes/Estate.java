01fe34c0d77a0758b44ffa84070a63121dcbcc29
simonkrupa <xkrupas@stuba.sk>
2021-03-19 16:55:38 +0100

    kupovanie pozemkov, penalty za statie

--- src/main/java/sk/stuba/fei/uim/oop/boxes/Estate.java
+++ src/main/java/sk/stuba/fei/uim/oop/boxes/Estate.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Estate extends Box{
     private final int penalty;
     private final int price;
+    private Player ownership = null;
     public Estate(int position){
         super(position);
         Random rand = new Random();
@@ -26,13 +27,30 @@ public class Estate extends Box{
     @Override
     public void trigger(Player player) {
         Scanner obj = new Scanner(System.in);
-        System.out.println(player.getName() + " is standing on estate, press 1 to buy or 0 to pass:");
-        int decision = obj.nextInt();
-        if (decision == 1){
-            System.out.println("You have decided to buy this property, cost is "+this.price);
-            player.setCash(-this.price);
-        } else{
-            System.out.println("You have decided to pass.");
+        System.out.println(player.getName() + " is standing on estate");
+        if(this.ownership!=null){
+            standingOnProperty(player);
+        }else {
+            System.out.println("the price is " + this.price + ", press 1 to buy or 0 to pass:");
+            int decision = obj.nextInt();
+            if (decision == 1) {
+                buyProperty(player);
+            } else {
+                System.out.println("You have decided to pass.");
+            }
         }
+    }
+
+    public void buyProperty(Player player){
+        System.out.println("You have decided to buy this property");
+        player.setCash(-this.price);
+        this.ownership = player;
+    }
+
+    public void standingOnProperty(Player player){
+        System.out.println("The owner of this property is " + this.ownership.getName() + ", you will have to pay penalty " +this.penalty +" for standing!");
+        player.setCash(-this.penalty);
+        this.ownership.setCash(this.penalty);
+        System.out.println(this.ownership.getName() + ", earned " + this.penalty + " from " + player.getName());
     }
 }
