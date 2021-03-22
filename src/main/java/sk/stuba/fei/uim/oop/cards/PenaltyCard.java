d01a4a9e1ced526a05a8f726420cbb06c63f3964
simonkrupa <xkrupas@stuba.sk>
2021-03-22 16:36:27 +0100

    3 karty vytvorene, este nepouzite v hracej ploche

---
+++ src/main/java/sk/stuba/fei/uim/oop/cards/PenaltyCard.java
@@ -0,0 +1,18 @@
+package sk.stuba.fei.uim.oop.cards;
+
+import sk.stuba.fei.uim.oop.players.Player;
+
+public class PenaltyCard extends Card{
+    private final int penaltyAmount = 5000;
+
+    public PenaltyCard(int position) {
+        super(position);
+    }
+
+    @Override
+    public void functionOfCard(Player player) {
+        System.out.println(player.getName() + ", sadly, you have exceeded the speed limit on your car.");
+        System.out.println("You must pay " + penaltyAmount);
+        player.setCash(-penaltyAmount);
+    }
+}
---
+++ src/main/java/sk/stuba/fei/uim/oop/cards/VacationCard.java
@@ -0,0 +1,18 @@
+package sk.stuba.fei.uim.oop.cards;
+
+import sk.stuba.fei.uim.oop.players.Player;
+
+public class VacationCard extends Card {
+    private final int vacationLength = 1;
+
+    public VacationCard(int position) {
+        super(position);
+    }
+
+    @Override
+    public void functionOfCard(Player player) {
+        System.out.println(player.getName() + ", you went on a vacation.");
+        System.out.println("You will rest " + vacationLength + " round on vacation, enjoy.");
+        player.setMove(false);
+    }
+}
